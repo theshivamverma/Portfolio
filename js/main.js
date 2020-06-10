330511866156226cfc2bd26dc242c93f8b3748a9
ShivamVerma22 <vermashivam527@gmail.com>
2020-06-10 06:13:35 +0530

    web_projects_update1

--- js/main.js
+++ js/main.js
@@ -1,23 +1,59 @@
 particlesJS.load('particles','js/particles.json');
 
 const c = document.querySelector("#uiux");
-f=0
+const cl = document.querySelector("#showcase");
+const b = document.querySelector("#blank");
+const wp = document.querySelector("#webprojects");
+
+var f=0
 c.addEventListener("click",function(){
-    const cl = document.querySelector("#showcase");
-    const b = document.querySelector("#blank");
         if(f == 0){
             cl.style.display = "block";
-            b.style.display = "none"
+            wp.style.display = "none";
+            b.style.display = "none";
             cl.style.display = "flex";
-            cl.style.justifyContent = "space-between"
+            cl.style.justifyContent = "space-between";
             f = 1;
+            f1 = 0;
         }
         else{
             cl.style.display = "none";
-            b.style.display = "block"
+            b.style.display = "block";
             f = 0;
         }
 })
+
+const w = document.querySelector("#web");
+var f1 = 0;
+w.addEventListener("click",function(){
+        if(f1 == 0){
+            wp.style.display = "block";
+            cl.style.display = "none";
+            b.style.display = "none";
+            f1 = 1;
+            f = 0;
+        }
+        else{
+            wp.style.display = "none";
+            b.style.display = "block";
+            f1 = 0;
+        }
+})
+
+var x = document.querySelectorAll(".websites");
+
+x.forEach(elem => {
+    elem.addEventListener("mouseenter", function(){
+        elem.innerHTML = `<button id="btn"> Visit Website</button><div id="top"></div>`
+        var top = document.querySelector("#top");
+        top.style.top = "0%"
+    })
+
+    elem.addEventListener("mouseleave", function() {
+        elem.innerHTML = ``;
+    })
+})
+
 
 const modal = document.querySelector("#modalpopup")
 const cont = document.querySelector("#cont")
@@ -28,8 +64,8 @@ function openmodal(path){
 }
 
 
-const cl = document.querySelector("#cl");
-cl.addEventListener("click",function(){
+const cls = document.querySelector("#cl");
+cls.addEventListener("click",function(){
     modal.style.display = "none";
 })
 
