0209db2633da65e4eb2f654402406a0443e35196
Shivam Verma <vermashivam527@gmail.com>
2021-01-13 01:31:12 +0530

    portfolio v2.0

--- js/main.js
+++ js/main.js
@@ -1,111 +1,169 @@
 particlesJS.load('particles','js/particles.json');
+particlesJS.load('bio','js/particles.json');
+particlesJS.load('projects','js/particles.json');
 
-const c = document.querySelector("#uiux");
-const cl = document.querySelector("#showcase");
-const b = document.querySelector("#blank");
-const wp = document.querySelector("#webprojects");
+import projects from './projects.js'
 
-var f=0
-c.addEventListener("click",function(){
-        if(f == 0){
-            cl.style.display = "block";
-            wp.style.display = "none";
-            b.style.display = "none";
-            cl.style.display = "flex";
-            cl.style.justifyContent = "space-between";
-            f = 1;
-            f1 = 0;
-        }
-        else{
-            cl.style.display = "none";
-            b.style.display = "block";
-            f = 0;
-        }
-})
+let hcj = document.querySelector("#hcj");
+let react = document.querySelector("#react");
+let mern = document.querySelector("#mern");
 
-const w = document.querySelector("#web");
-var f1 = 0;
-w.addEventListener("click",function(){
-        if(f1 == 0){
-            wp.style.display = "block";
-            cl.style.display = "none";
-            b.style.display = "none";
-            f1 = 1;
-            f = 0;
-        }
-        else{
-            wp.style.display = "none";
-            b.style.display = "block";
-            f1 = 0;
-        }
-})
+var selected = "hcj";
 
-var x = document.querySelectorAll(".static");
-
-x.forEach((elem, index) => {
-    elem.addEventListener("mouseenter", function(){
-        elem.innerHTML = `<a target="_blank" href="https://project${index + 1}web.netlify.app"><button id="btn"> Visit Website</button></a><div id="top"></div>`
-        var top = document.querySelector("#top");
-        top.style.top = "0%"
-    })
-
-    console.log(index)
-
-    elem.addEventListener("mouseleave", function() {
-        elem.innerHTML = ``;
-    })
-})
-
-var y = document.querySelectorAll(".anim");
-
-y.forEach((elem, index) => {
-    elem.addEventListener("mouseenter", function(){
-        elem.innerHTML = `<a target="_blank" href="https://shivamwebanim${index + 1}.netlify.app"><button id="btn"> Visit Website</button></a><div id="top"></div>`
-        var top = document.querySelector("#top");
-        top.style.top = "0%"
-    })
-
-    console.log(index)
-
-    elem.addEventListener("mouseleave", function() {
-        elem.innerHTML = ``;
-    })
-})
-
-var z = document.querySelectorAll(".resp");
-
-z.forEach((elem, index) => {
-    elem.addEventListener("mouseenter", function(){
-        elem.innerHTML = `<a target="_blank" href="https://shivamwebresp${index + 1}.netlify.app"><button id="btn"> Visit Website</button></a><div id="top"></div>`
-        var top = document.querySelector("#top");
-        top.style.top = "0%"
-    })
-
-    console.log(index)
-
-    elem.addEventListener("mouseleave", function() {
-        elem.innerHTML = ``;
-    })
-})
-
-
-const modal = document.querySelector("#modalpopup")
-const cont = document.querySelector("#cont")
-
-function openmodal(path){
-    modal.style.display = "block";
-    cont.src = path;
+function displayProjects() {
+  let currentSelection = projects[selected];
+  document.querySelector(".projects").style.height =
+    100 + currentSelection.length * 26 + "vh";
+  currentSelection.forEach(function (p) {
+    let labelstring = p.labels.map(function (l) {
+      return `<span class="label ${l}">${l}</span>`;
+    });
+    document.querySelector(".project-container").innerHTML =
+      document.querySelector(".project-container").innerHTML +
+      `<div class="project">
+          <img class="project-image" src="${p.img}" alt="">
+          <div class="project-desc">
+            <p>${p.desc}</p>
+            ${labelstring.join(" ")}
+            <a href="${p.livelink}" target="_blank">
+              <button>Visit Live Project<i class="ri-window-fill"></i></button>
+            </a>
+            ${
+              p.source !== undefined
+                ? `<a href="${p.source}">
+              <button>View Source Code <i class="ri-github-fill"></i></button>
+            </a>`
+                : ""
+            }
+          </div>
+        </div>`;
+  });
 }
 
-
-const cls = document.querySelector("#cl");
-cls.addEventListener("click",function(){
-    modal.style.display = "none";
+window.addEventListener('load', function() {
+    displayProjects();
 })
 
-
-window.addEventListener("click",function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
+hcj.addEventListener('click', function(){
+  document.querySelector(".project-container").innerHTML = ``;
+  selected = "hcj";
+  displayProjects();
 })
+
+react.addEventListener('click', function(){
+  document.querySelector('.project-container').innerHTML = ``;
+  selected = "react";
+  displayProjects();
+})
+
+// const c = document.querySelector("#uiux");
+// const cl = document.querySelector("#showcase");
+// const b = document.querySelector("#blank");
+// const wp = document.querySelector("#webprojects");
+
+// var f=0
+// c.addEventListener("click",function(){
+//         if(f == 0){
+//             cl.style.display = "block";
+//             wp.style.display = "none";
+//             b.style.display = "none";
+//             cl.style.display = "flex";
+//             cl.style.justifyContent = "space-between";
+//             f = 1;
+//             f1 = 0;
+//         }
+//         else{
+//             cl.style.display = "none";
+//             b.style.display = "block";
+//             f = 0;
+//         }
+// })
+
+// const w = document.querySelector("#web");
+// var f1 = 0;
+// w.addEventListener("click",function(){
+//         if(f1 == 0){
+//             wp.style.display = "block";
+//             cl.style.display = "none";
+//             b.style.display = "none";
+//             f1 = 1;
+//             f = 0;
+//         }
+//         else{
+//             wp.style.display = "none";
+//             b.style.display = "block";
+//             f1 = 0;
+//         }
+// })
+
+// var x = document.querySelectorAll(".static");
+
+// x.forEach((elem, index) => {
+//     elem.addEventListener("mouseenter", function(){
+//         elem.innerHTML = `<a target="_blank" href="https://project${index + 1}web.netlify.app"><button id="btn"> Visit Website</button></a><div id="top"></div>`
+//         var top = document.querySelector("#top");
+//         top.style.top = "0%"
+//     })
+
+//     console.log(index)
+
+//     elem.addEventListener("mouseleave", function() {
+//         elem.innerHTML = ``;
+//     })
+// })
+
+// var y = document.querySelectorAll(".anim");
+
+// y.forEach((elem, index) => {
+//     elem.addEventListener("mouseenter", function(){
+//         elem.innerHTML = `<a target="_blank" href="https://shivamwebanim${index + 1}.netlify.app"><button id="btn"> Visit Website</button></a><div id="top"></div>`
+//         var top = document.querySelector("#top");
+//         top.style.top = "0%"
+//     })
+
+//     console.log(index)
+
+//     elem.addEventListener("mouseleave", function() {
+//         elem.innerHTML = ``;
+//     })
+// })
+
+// var z = document.querySelectorAll(".resp");
+
+// z.forEach((elem, index) => {
+//     elem.addEventListener("mouseenter", function(){
+//         elem.innerHTML = `<a target="_blank" href="https://shivamwebresp${index + 1}.netlify.app"><button id="btn"> Visit Website</button></a><div id="top"></div>`
+//         var top = document.querySelector("#top");
+//         top.style.top = "0%"
+//     })
+
+//     console.log(index)
+
+//     elem.addEventListener("mouseleave", function() {
+//         elem.innerHTML = ``;
+//     })
+// })
+
+
+// const modal = document.querySelector("#modalpopup")
+// const cont = document.querySelector("#cont")
+
+// function openmodal(path){
+//     modal.style.display = "block";
+//     cont.src = path;
+// }
+
+
+// const cls = document.querySelector("#cl");
+// cls.addEventListener("click",function(){
+//     modal.style.display = "none";
+// })
+
+
+// window.addEventListener("click",function(event) {
+//     if (event.target == modal) {
+//         modal.style.display = "none";
+//     }
+// })
+
+// "value": "#1cd0dc",
---
+++ js/projects.js
@@ -0,0 +1,53 @@
+const projects = {
+  hcj: [
+    {
+      img: "./projects/hcj1.png",
+      desc: "A sample dashboard",
+      labels: ["html", "css", "js"],
+      livelink: "https://dashboard-design-sv.netlify.app/",
+      source: "https://github.com/ShivamVerma22/dashboard-design",
+    },
+    {
+      img: "./projects/hcj2.png",
+      desc:
+        "A website made for an advertising company while working there as a frontend developer intern.",
+      labels: ["html", "css", "js"],
+      livelink: "https://offiql.com",
+    },
+    {
+      img: "./projects/hcj3.png",
+      desc:
+        "A web app that works as a cash register, takes input of bill amount and cash received and tells to return the balance in lowest number of denominations possible.",
+      labels: ["html", "css", "js"],
+      livelink: "https://cashregisterpro.netlify.app",
+      source: "https://github.com/ShivamVerma22/cashRegister",
+    },
+    {
+      img: "./projects/hcj4.png",
+      desc:
+        "A web app that translates normal english to funny minion language using API by funtranslations.com",
+      labels: ["html", "css", "js"],
+      livelink: "https://minionlanguage.netlify.app/",
+      source: "https://github.com/ShivamVerma22/bananaSpeak",
+    },
+  ],
+  react: [
+    {
+      img: "./projects/r1.png",
+      desc: "A web app that works as a dictionary for emojis",
+      labels: ["html", "css", "react"],
+      livelink: "https://react-emoji-dictionary.netlify.app/",
+      source: "https://github.com/ShivamVerma22/emojiDictionaryReact",
+    },
+    {
+      img: "./projects/r2.png",
+      desc:
+        "A web app that provides a very fun interactive way to learn about triangles.",
+      labels: ["html", "css", "react"],
+      livelink: "https://fun-learning-triangles.netlify.app/",
+      source: "https://github.com/ShivamVerma22/geometryApp",
+    },
+  ],
+};
+
+export default projects;
